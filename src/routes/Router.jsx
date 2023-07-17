21806aa7643d216673907fa10b070e609423de15
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-07-17 21:48:21 +0600

    Added home route and MainLayout

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Auth/Login/Login";
+import Register from "../pages/Auth/Register/Register";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/register',
+                element: <Register />
+            }
+        ]
     },
 ]);
 
