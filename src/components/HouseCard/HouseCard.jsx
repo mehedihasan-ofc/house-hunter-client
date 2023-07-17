7db401d3cf090939a5407dfa951adfece02ba7e8
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-07-18 01:16:54 +0600

    Added House list

---
+++ src/components/HouseCard/HouseCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HouseCard = ({house}) => {
+
+    console.log(house);
+    return (
+        <div>
+            HouseCard
+        </div>
+    );
+};
+
+export default HouseCard;
---
+++ src/pages/Home/Houses/Houses.jsx
@@ -0,0 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import HouseCard from '../../../components/HouseCard/HouseCard';
+
+const Houses = () => {
+
+    const { data: houses = [], isLoading } = useQuery({
+        queryKey: ['houses'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/houses');
+            return res.json();
+        }
+    });
+
+    return (
+        <div className='max-w-7xl w-full mx-auto px-6 my-10'>
+            <div className='text-center'>
+                <h2 className='text-4xl font-semibold'>The Best Way To Find Your House</h2>
+                <p className='text-xl font-light mt-2'>Help your visitors find the right property by letting them set various search criteria</p>
+            </div>
+            {/* Search box */}
+
+
+            <div>
+                {
+                    houses.map(house => <HouseCard key={house._id} house={house} />)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Houses;
