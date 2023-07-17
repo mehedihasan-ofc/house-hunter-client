93eeb05cd0eb08825c025d46cd5a7fabe00bf4f5
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-07-18 02:49:06 +0600

    added register page using - axios react hook form

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
-const RegisterForm = () => {
+const Register = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = (data) => {
@@ -10,9 +11,9 @@ const RegisterForm = () => {
     };
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
-                <h2 className="text-2xl mb-6">Registration</h2>
+        <div className="flex justify-center items-center h-full my-5">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-2/5 border" onSubmit={handleSubmit(onSubmit)}>
+                <h2 className="text-2xl text-center mb-6">Registration</h2>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="fullName">
                         Full Name
@@ -26,33 +27,35 @@ const RegisterForm = () => {
                     />
                     {errors.fullName && <p className="text-red-500 text-xs mt-1">Full Name is required.</p>}
                 </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="role">
-                        Role
-                    </label>
-                    <select
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="role"
-                        {...register('role', { required: true })}
-                    >
-                        <option value="">Select your role</option>
-                        <option value="House Owner">House Owner</option>
-                        <option value="House Renter">House Renter</option>
-                    </select>
-                    {errors.role && <p className="text-red-500 text-xs mt-1">Role is required.</p>}
-                </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phoneNumber">
-                        Phone Number
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="phoneNumber"
-                        type="tel"
-                        placeholder="Enter your phone number"
-                        {...register('phoneNumber', { required: true })}
-                    />
-                    {errors.phoneNumber && <p className="text-red-500 text-xs mt-1">Phone Number is required.</p>}
+                <div className="flex justify-between">
+                    <div className="w-full mb-4 mr-2">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="role">
+                            Role
+                        </label>
+                        <select
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="role"
+                            {...register('role', { required: true })}
+                        >
+                            <option value="">Select your role</option>
+                            <option value="House Owner">House Owner</option>
+                            <option value="House Renter">House Renter</option>
+                        </select>
+                        {errors.role && <p className="text-red-500 text-xs mt-1">Role is required.</p>}
+                    </div>
+                    <div className="w-full mb-4 ml-2">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phoneNumber">
+                            Phone Number
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="phoneNumber"
+                            type="tel"
+                            placeholder="Enter your phone number"
+                            {...register('phoneNumber', { required: true })}
+                        />
+                        {errors.phoneNumber && <p className="text-red-500 text-xs mt-1">Phone Number is required.</p>}
+                    </div>
                 </div>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -82,15 +85,17 @@ const RegisterForm = () => {
                 </div>
                 <div className="flex items-center justify-between">
                     <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         type="submit"
                     >
                         Register
                     </button>
                 </div>
+
+                <p className="block text-gray-700 text-center text-sm font-bold mt-2">Already have an account? <Link to='/login'>Login</Link></p>
             </form>
         </div>
     );
 };
 
-export default RegisterForm;
+export default Register;
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   };
 
   return (
-    <nav className={location.pathname === '/' ? 'absolute top-0 left-0 w-full text-white py-2 px-6 z-10' : 'bg-green-600 py-2'}>
+    <nav className={location.pathname === '/' ? 'absolute top-0 left-0 w-full text-white py-2 px-6 z-10' : 'bg-green-600'}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Mobile Menu Toggle */}
