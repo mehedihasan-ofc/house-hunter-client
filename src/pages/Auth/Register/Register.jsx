20ca38136f362b0d5a6efbb4ce4397a400c204b0
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-07-18 02:21:34 +0600

    install react-hook-form - Performant, flexible and extensible forms with easy-to-use validation

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,121 +1,96 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const RegisterForm = () => {
-  const [fullName, setFullName] = useState('');
-  const [role, setRole] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const newUser = {
-      fullName,
-      role,
-      phoneNumber,
-      email,
-      password,
+    const onSubmit = (data) => {
+        console.log(data);
+        // Perform form submission, validation, and API calls here
     };
 
-    try {
-      const response = await axios.post('/api/register', newUser);
-      console.log(response.data); // Assuming the response contains the JWT token
-      // Redirect or perform other actions upon successful registration
-    } catch (error) {
-      console.error(error);
-      // Handle error during registration
-    }
-  };
-
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-        <h2 className="text-2xl mb-6">Registration</h2>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="fullName">
-            Full Name
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="fullName"
-            type="text"
-            placeholder="Enter your full name"
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
-            required
-          />
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
+                <h2 className="text-2xl mb-6">Registration</h2>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="fullName">
+                        Full Name
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="fullName"
+                        type="text"
+                        placeholder="Enter your full name"
+                        {...register('fullName', { required: true })}
+                    />
+                    {errors.fullName && <p className="text-red-500 text-xs mt-1">Full Name is required.</p>}
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="role">
+                        Role
+                    </label>
+                    <select
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="role"
+                        {...register('role', { required: true })}
+                    >
+                        <option value="">Select your role</option>
+                        <option value="House Owner">House Owner</option>
+                        <option value="House Renter">House Renter</option>
+                    </select>
+                    {errors.role && <p className="text-red-500 text-xs mt-1">Role is required.</p>}
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phoneNumber">
+                        Phone Number
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="phoneNumber"
+                        type="tel"
+                        placeholder="Enter your phone number"
+                        {...register('phoneNumber', { required: true })}
+                    />
+                    {errors.phoneNumber && <p className="text-red-500 text-xs mt-1">Phone Number is required.</p>}
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        placeholder="Enter your email"
+                        {...register('email', { required: true, pattern: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/ })}
+                    />
+                    {errors.email && <p className="text-red-500 text-xs mt-1">Email is required and must be valid.</p>}
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        placeholder="Enter your password"
+                        {...register('password', { required: true })}
+                    />
+                    {errors.password && <p className="text-red-500 text-xs mt-1">Password is required.</p>}
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                    >
+                        Register
+                    </button>
+                </div>
+            </form>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="role">
-            Role
-          </label>
-          <select
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="role"
-            value={role}
-            onChange={(e) => setRole(e.target.value)}
-            required
-          >
-            <option value="">Select your role</option>
-            <option value="House Owner">House Owner</option>
-            <option value="House Renter">House Renter</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phoneNumber">
-            Phone Number
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="phoneNumber"
-            type="tel"
-            placeholder="Enter your phone number"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Register
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default RegisterForm;
