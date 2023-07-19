c2216dc8b4d544ac7e5f99b292015a73ded3005a
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-07-19 11:21:52 +0600

    Added house list

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -12,7 +12,7 @@ const DashboardLayout = () => {
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
+            <div className="drawer-content bg-[#F4F7FE] flex flex-col items-center">
                 {/* Page content here */}
 
                 <Outlet />
@@ -23,7 +23,7 @@ const DashboardLayout = () => {
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
 
-                <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                <ul className="menu p-4 w-80 h-full bg-white text-base-content">
                     {/* Sidebar content here */}
 
                     {isHouseOwner ? <HouseOwnerMenu /> : <HouseRenterMenu />}
--- src/pages/Dashboard/HouseList/HouseList.jsx
+++ src/pages/Dashboard/HouseList/HouseList.jsx
@@ -1,9 +1,88 @@
 import React from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const HouseList = () => {
+
+    const email = localStorage.getItem('access-email')
+    const [axiosSecure] = useAxiosSecure();
+
+    const { data: houses = [], refetch } = useQuery({
+        queryKey: ['houses', email],
+        enabled: !!email && !!localStorage.getItem('access-token'),
+        queryFn: async () => {
+            const res = await axiosSecure(`/my-houses?email=${email}`)
+            return res.data;
+        }
+    })
+
+    console.log(houses);
+
     return (
-        <div>
-            
+        <div className='my-container my-5'>
+            <div className="overflow-x-auto bg-white shadow-md">
+                <table className="table">
+                    {/* head */}
+                    <thead className='uppercase'>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>House Name</th>
+                            <th>RPM</th>
+                            <th>AVAIL. Date</th>
+                            <th>br</th>
+                            <th>BTHRM</th>
+                            <th>City</th>
+                            <th>address</th>
+                            {/* <th>Feedback</th> */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            houses.map((house, idx) => <tr key={house._id}>
+                                <td>
+                                    {idx + 1}
+                                </td>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={house.picture} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{house.name}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <span className="badge badge-ghost badge-sm">${house.rent_per_month}</span>
+                                </td>
+                                <td>
+                                    <span className='badge badge-accent badge-outline'>{house.availability_date}</span>
+                                </td>
+                                <td>
+                                    <span className='badge badge-primary badge-outline'>{house.bedrooms}</span>
+                                </td>
+                                <td>
+                                    <span className='badge badge-secondary badge-outline'>{house.bathrooms}</span>
+                                </td>
+                                <td>
+                                    <span className='badge badge-secondary badge-outline'>{house.city}</span>
+                                </td>
+                                <td>
+                                    <span className='badge badge-secondary badge-outline'>{house.address}</span>
+                                </td>
+                                {/* {myClass.feedback && <td>
+                                    <button className="btn btn-ghost btn-xs">{myClass.feedback}</button>
+                                </td>} */}
+                            </tr>)
+                        }
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
